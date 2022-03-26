c3db8fcc4eb6c190e8e19bf27e5c8a8f2490795c
NukerFall <47152422+NukerFall@users.noreply.github.com>
2022-03-26 22:48:40 +0300

    Update Main.java

--- src/me/FortiBrine/JustChats/main/Main.java
+++ src/me/FortiBrine/JustChats/main/Main.java
@@ -1,27 +1,27 @@
 package me.FortiBrine.JustChats.main;
 
 import java.io.File;
-
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import me.FortiBrine.JustChats.commands.ReloadCommand;
 import me.FortiBrine.JustChats.listeners.MainListener;
 
 public class Main extends JavaPlugin {
 
-	public File messageFile = new File(getDataFolder()+File.separator+"messages.yml");
-	
-	@Override
+	private File locale = new File(getDataFolder(), "locale.yml");
+	private FileConfiguration lconf;
+
 	public void onEnable() {
-		File config = new File(getDataFolder()+File.separator+"config.yml");
-		if (!config.exists()) {
-			getConfig().options().copyDefaults(true);
-			saveDefaultConfig();
+		saveDefaultConfig();
+		reloadLocale();
+		new ReloadCommand(this);
+		new MainListener(this);
+	}
+	
+	public void reloadLocale() {
+		if (!locale.exists()) {
+			saveResource("locale.yml", false);	
 		}
-		if (!messageFile.exists()) saveResource("messages.yml", false);
-		
-		this.getCommand("jcreload").setExecutor(new ReloadCommand(this));
-		Bukkit.getPluginManager().registerEvents(new MainListener(this), this);
+		lconf = YamlConfiguration.loadConfiguration(locale);
 	}
 }
